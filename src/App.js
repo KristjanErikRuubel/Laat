ef4f64ae9fdcff870779af53ec54fb4961dd11dc
unknown <kristjan-e@hotmail.com>
2020-03-06 16:01:57 +0200

    LAAT

---
+++ src/App.js
@@ -0,0 +1,165 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <body className="background">
+      
+      <div className="container">
+            <div className="box">
+                <div className="title">
+                    <span className="block"></span>
+                    <h1>Laat<span></span></h1>
+                </div>
+                <div className="role">
+                    <div className="block"></div>
+                    <p>Greatest webshop builder in 2020</p>
+                </div>
+            </div>
+        </div>
+        <div className="content"> 
+        <div className="header bubble-wobble">
+            <img className="image bubble-wobble" src="happy.jpg"alt="eshop" height="300" width="500"></img>
+              <h1>
+                Create your webshop in 30 seconds
+              </h1>
+              <ul className="">
+                <li>No coding or design experience required</li>
+                <li>Your own domain name</li>
+                <li>Sell items from your own website</li>
+                <li>Integration with credit cards and other online payment methods</li>
+              </ul>
+              <div id="container">
+                <div className="darkBlue">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="74.248" height="56.786" viewBox="0 0 74.248 56.786">
+                      <path id="darkBlue" data-name="Path 11" d="M238.113,106.948H200.989l-18.562,28.393,18.562,28.393h37.124l18.562-28.393Z" transform="translate(-182.427 -106.948)" fill="#364f6b"/>
+                  </svg>
+                </div>
+                <div className="blue">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="74.248" height="56.786" viewBox="0 0 74.248 56.786">
+                    <path id="blue" data-name="Path 12" d="M225.28,106.948H188.156l-18.562,28.393,18.562,28.393H225.28l18.562-28.393Z" transform="translate(-169.594 -106.948)" fill="#3fc1c9"/>
+                  </svg>
+                </div>
+
+                <div className="button">
+                  <span className="text">Start now</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="253.813" height="57.79" viewBox="0 0 253.813 57.79">
+                    <g id="Group_42" data-name="Group 42" transform="translate(-356 -428.807)">
+                      <path id="Path_13" data-name="Path 13" d="M87.837,106.948H50.713l-18.562,28.9,18.562,28.9H87.837l18.562-28.9Z" transform="translate(323.849 321.858)" fill="#e91e63"/>
+                        <rect id="Rectangle_11" data-name="Rectangle 11" width="216.747" height="57.79" transform="translate(374.504 428.807)" fill="#e91e63"/>
+                      <path id="Path_14" data-name="Path 14" d="M212.367,106.948H175.243l-18.562,28.9,18.562,28.9h37.124l18.562-28.9Z" transform="translate(378.884 321.858)" fill="#e91e63"/>
+                    </g>
+                  </svg>
+                </div>
+              </div>
+        </div>
+        <div>
+
+          <div className="container-2 bubble-wobble">
+            <h1>
+              Create a unique experience for your customers
+            </h1>
+            <ul>
+                <li>Get your unique domain name</li>
+                <li>Stunning templates</li>
+              </ul>
+            <div id="container">
+                <div className="darkBlue">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="74.248" height="56.786" viewBox="0 0 74.248 56.786">
+                      <path id="darkBlue" data-name="Path 11" d="M238.113,106.948H200.989l-18.562,28.393,18.562,28.393h37.124l18.562-28.393Z" transform="translate(-182.427 -106.948)" fill="#364f6b"/>
+                  </svg>
+                </div>
+                <div className="blue">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="74.248" height="56.786" viewBox="0 0 74.248 56.786">
+                    <path id="blue" data-name="Path 12" d="M225.28,106.948H188.156l-18.562,28.393,18.562,28.393H225.28l18.562-28.393Z" transform="translate(-169.594 -106.948)" fill="#3fc1c9"/>
+                  </svg>
+                </div>
+
+                <div className="button">
+                  <span className="text">Start now</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="253.813" height="57.79" viewBox="0 0 253.813 57.79">
+                    <g id="Group_42" data-name="Group 42" transform="translate(-356 -428.807)">
+                      <path id="Path_13" data-name="Path 13" d="M87.837,106.948H50.713l-18.562,28.9,18.562,28.9H87.837l18.562-28.9Z" transform="translate(323.849 321.858)" fill="#e91e63"/>
+                        <rect id="Rectangle_11" data-name="Rectangle 11" width="216.747" height="57.79" transform="translate(374.504 428.807)" fill="#e91e63"/>
+                      <path id="Path_14" data-name="Path 14" d="M212.367,106.948H175.243l-18.562,28.9,18.562,28.9h37.124l18.562-28.9Z" transform="translate(378.884 321.858)" fill="#e91e63"/>
+                    </g>
+                  </svg>
+                </div>
+              </div>
+          </div>
+        </div>
+        <div>
+
+          <div className="container-3 bubble-wobble">
+              <h1>
+                Explore all the possibilities!
+              </h1>
+              <ul>
+                <li>Customize colors, logo, images, and more</li>
+                <li>User-friendly website builder</li>
+              </ul>
+              <div id="container">
+                <div className="darkBlue">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="74.248" height="56.786" viewBox="0 0 74.248 56.786">
+                      <path id="darkBlue" data-name="Path 11" d="M238.113,106.948H200.989l-18.562,28.393,18.562,28.393h37.124l18.562-28.393Z" transform="translate(-182.427 -106.948)" fill="#364f6b"/>
+                  </svg>
+                </div>
+                <div className="blue">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="74.248" height="56.786" viewBox="0 0 74.248 56.786">
+                    <path id="blue" data-name="Path 12" d="M225.28,106.948H188.156l-18.562,28.393,18.562,28.393H225.28l18.562-28.393Z" transform="translate(-169.594 -106.948)" fill="#3fc1c9"/>
+                  </svg>
+                </div>
+
+                <div className="button">
+                  <span className="text">Start now</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="253.813" height="57.79" viewBox="0 0 253.813 57.79">
+                    <g id="Group_42" data-name="Group 42" transform="translate(-356 -428.807)">
+                      <path id="Path_13" data-name="Path 13" d="M87.837,106.948H50.713l-18.562,28.9,18.562,28.9H87.837l18.562-28.9Z" transform="translate(323.849 321.858)" fill="#e91e63"/>
+                        <rect id="Rectangle_11" data-name="Rectangle 11" width="216.747" height="57.79" transform="translate(374.504 428.807)" fill="#e91e63"/>
+                      <path id="Path_14" data-name="Path 14" d="M212.367,106.948H175.243l-18.562,28.9,18.562,28.9h37.124l18.562-28.9Z" transform="translate(378.884 321.858)" fill="#e91e63"/>
+                    </g>
+                  </svg>
+                </div>
+              </div>
+            </div>
+        </div>
+        <div>
+          <div className="container-4 bubble-wobble">
+                <h1>
+                  Set up your own webshop in 30 seconds
+                </h1>
+                <ul>
+                <li>Setting up is easy</li>
+                <li>Register Now and get one month hosting for free!</li>
+              </ul>
+                <div id="container">
+                <div className="darkBlue">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="74.248" height="56.786" viewBox="0 0 74.248 56.786">
+                      <path id="darkBlue" data-name="Path 11" d="M238.113,106.948H200.989l-18.562,28.393,18.562,28.393h37.124l18.562-28.393Z" transform="translate(-182.427 -106.948)" fill="#364f6b"/>
+                  </svg>
+                </div>
+                <div className="blue">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="74.248" height="56.786" viewBox="0 0 74.248 56.786">
+                    <path id="blue" data-name="Path 12" d="M225.28,106.948H188.156l-18.562,28.393,18.562,28.393H225.28l18.562-28.393Z" transform="translate(-169.594 -106.948)" fill="#3fc1c9"/>
+                  </svg>
+                </div>
+
+                <div className="button">
+                  <span className="text">Start now</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="253.813" height="57.79" viewBox="0 0 253.813 57.79">
+                    <g id="Group_42" data-name="Group 42" transform="translate(-356 -428.807)">
+                      <path id="Path_13" data-name="Path 13" d="M87.837,106.948H50.713l-18.562,28.9,18.562,28.9H87.837l18.562-28.9Z" transform="translate(323.849 321.858)" fill="#e91e63"/>
+                        <rect id="Rectangle_11" data-name="Rectangle 11" width="216.747" height="57.79" transform="translate(374.504 428.807)" fill="#e91e63"/>
+                      <path id="Path_14" data-name="Path 14" d="M212.367,106.948H175.243l-18.562,28.9,18.562,28.9h37.124l18.562-28.9Z" transform="translate(378.884 321.858)" fill="#e91e63"/>
+                    </g>
+                  </svg>
+                </div>
+              </div>
+              </div>
+          </div>
+        </div>
+    </body>
+  );
+}
+
+export default App;
